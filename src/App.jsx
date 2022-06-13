538833719ccb9c42cbd5081930e8064e43e3b087
omer <peynirciomerfaruk@gmail.com>
2022-06-13 23:41:33 +0200

    react bootstrap in

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-
+import Button from "react-bootstrap/Button";
 function App() {
- 
-
   return (
     <div className="App">
-      
+      <div className="container">
+        <h1>Company Site</h1>
+        <Button className="me-2">Employees</Button>
+        <Button className="btn-success me-1">Customers</Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
