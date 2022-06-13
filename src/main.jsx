257c6ccf05b195a5570f6849e1613f362787f6de
omer <peynirciomerfaruk@gmail.com>
2022-06-13 23:46:25 +0200

    bootswatch templates in

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/bootstrap.darkly.min.css";
+import "../styles/bootstrap.quartz.min.css";
+import "../styles/bootstrap.morph.min.css";
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
